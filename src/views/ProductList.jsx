dd02971580e44ed68f8580e5c3b4f72ac594cabd
ianSu <iamsu@mirle.com.tw>
2023-08-17 08:56:00 +0800

    product list add categories link

--- src/views/ProductList.jsx
+++ src/views/ProductList.jsx
@@ -1,29 +1,47 @@
 import { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { useAuth, useCart } from '../hooks'
 
 const apiUrl = `${import.meta.env.VITE_API_URL}/api`
 
 const ProductList = () => {
+  const [categories, setCategories] = useState([])
   const [products, setProducts] = useState([])
   const [loading, setLoading] = useState(true)
   const { accessToken } = useAuth()
   const { add } = useCart()
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
+  const selectedCategoryId = queryParams.get('categoryId')
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const res = await fetch(`${apiUrl}/product`)
+        const res = await fetch(`${apiUrl}/category`)
         const data = await res.json()
-        setProducts(data)
-        setLoading(false)
+        setCategories(data)
       } catch (error) {
-        console.error('Error fetching data:', error)
-        setLoading(false)
+        console.error('Error fetching category data:', error)
       }
     }
     fetchData()
   }, [])
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const categoryQuery = selectedCategoryId ? `?categoryId=${selectedCategoryId}` : ''
+        const res = await fetch(`${apiUrl}/product${categoryQuery}`)
+        const data = await res.json()
+        setProducts(data)
+        setLoading(false)
+      } catch (error) {
+        console.error('Error fetching product data:', error)
+        setLoading(false)
+      }
+    }
+    fetchData()
+  }, [selectedCategoryId])
 
   const handleAddToCart = (product) => {
     add(product)
@@ -35,7 +53,19 @@ const ProductList = () => {
 
   return (
     <div>
-      <h2>List</h2>
+      <nav className="sub-nav">
+        <Link to="/product" className={selectedCategoryId ? '' : 'active'}>All</Link>
+        {
+          categories && categories.map(it => (
+            <Link
+              key={it.id}
+              to={`/product?categoryId=${it.id}`}
+              className={it.id === selectedCategoryId ? 'active' : ''}
+            >{it.name}
+            </Link>
+          ))
+        }
+      </nav>
       <div className="products">
         {products && products.map((product) => (
           <div className="product-box" key={product.id}>
